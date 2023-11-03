1dec661be840262f98c481910c8e98d4ce02f2f0
jh-488 <jihadhyadi01@gmail.com>
2023-11-03 11:12:37 +0100

    added page components

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <img src='/assets/logo.png' alt='little lemon logo'/>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Reservations</a></li>
+        <li><a href="#">Order Online</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
